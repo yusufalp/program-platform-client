b8bfcdddaace06e87303f69e76ae724481bc3159
yusufalp <alpyusuf007@gmail.com>
2025-05-20 21:30:03 -0700

    add profile interface

---
+++ src/types/profile.ts
@@ -0,0 +1,29 @@
+export interface Profile {
+  userId: string;
+  bio?: string;
+  dateOfBirth?: string;
+  phoneNumber: string;
+  status: "active" | "inactive" | "graduated";
+  cohort: number;
+  graduationDate: string;
+
+  address?: {
+    street?: {
+      line1?: string;
+      line2?: string;
+    };
+    city?: string;
+    state?: string;
+    postalCode?: string;
+    country?: string;
+  };
+
+  socials?: {
+    linkedin?: string;
+    github?: string;
+    website?: string;
+  };
+
+  createdAt?: string;
+  updatedAt?: string;
+}

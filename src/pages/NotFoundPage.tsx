5b75cbe6f175786d6768f06e2deb717e2af5bd7b
yusufalp <alpyusuf007@gmail.com>
2025-06-01 22:12:58 -0700

    update routing

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -2,17 +2,15 @@ import { useNavigate } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
 
 export default function NotFoundPage() {
-  const auth = useAuth();
+  const { token } = useAuth();
   const navigate = useNavigate();
 
   return (
     <div>
       <h2>404 - Page not found</h2>
       <p>The page you are looking for does not exist or moved</p>
-      {auth.token ? (
-        <button onClick={() => navigate(`/dashboard/${auth.user?._id}`)}>
-          Go to dashboard
-        </button>
+      {token ? (
+        <button onClick={() => navigate("/dashboard")}>Go to dashboard</button>
       ) : (
         <button onClick={() => navigate("/login")}>Go to login</button>
       )}

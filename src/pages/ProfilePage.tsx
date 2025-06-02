64d174a63e5f21b23d1f818b6a458568654fe677
yusufalp <alpyusuf007@gmail.com>
2025-06-01 22:13:34 -0700

    fetch profile on profile page

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,38 +1,8 @@
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-
+import Profile from "../features/profile/components/Profile";
 import { useAuth } from "../hooks/useAuth";
 
-import Profile from "../features/profile/components/Profile";
-import hasProfile from "../lib/checkProfile";
-
 export default function ProfilePage() {
-  const auth = useAuth();
-  const navigate = useNavigate();
-
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const verifyProfile = async () => {
-      if (!auth.user || !auth.token) return;
-
-      try {
-        const profileExists = await hasProfile(auth.user._id, auth.token);
-
-        if (!profileExists) {
-          navigate("/profile-form");
-        }
-      } catch (error) {
-        console.log(error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    verifyProfile();
-  }, [auth.token, auth.user, navigate]);
-
-  if (loading) return <p>Loading...</p>;
+  const { user } = useAuth();
 
   const roleProfiles: Record<string, React.FC> = {
     admin: Profile,
@@ -40,8 +10,8 @@ export default function ProfilePage() {
     // mentor: MentorProfile
   };
 
-  const RoleProfile = auth.user
-    ? roleProfiles[auth.user.role] ??
+  const RoleProfile = user
+    ? roleProfiles[user.role] ??
       (() => <p>No profile component found for your role.</p>)
     : () => <p>You must be logged in to view this page.</p>;
 

bbcffeab20c0cf43852f7920a5b4f7e68b7bf0b5
yusufalp <alpyusuf007@gmail.com>
2025-07-22 14:47:24 -0700

    add a user role

--- src/context/types/user.ts
+++ src/context/types/user.ts
@@ -4,7 +4,7 @@ export interface User {
   lastName: string;
   email: string;
   password?: string;
-  role: "admin" | "applicant";
+  role: "admin" | "applicant" | "mentor";
   createdAt?: string;
   updatedAt?: string;
 }
